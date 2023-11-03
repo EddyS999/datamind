1531ae19f8a3cad6cef6662df31145e0ace2f367
Akira <71152540+EddyS999@users.noreply.github.com>
2023-11-03 16:18:26 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,23 @@
-from datamind import SimpleLinearRegression as slr
+# Import 
+from datamind import SimpleLinearRegression
 from sklearn.datasets import make_regression
 import matplotlib .pyplot as plt 
 import numpy as np
 
-
-
-####### test du package  ######
-
+#test dataset
 np.random.seed(0)
 x,y = make_regression(n_samples=100, n_features = 1, noise = 10) 
 X = np.hstack((x, np.ones(x.shape)))#ajout d'un biais
 y = y.reshape(y.shape[0],1)
-modele = slr()
 
-final = modele.fit(X, y)
-cost_history = modele.cost_history(X, y)
-#print(final)
-#print(cost_history)
-prediction = modele.main_function(X, final)
-print(modele.score(y, prediction))
-'''
-plt.plot(range(1000), cost_history)
-plt.show()
+# Linear regression with fit()
+model = SimpleLinearRegression()
+teta_final = model.fit(X, y)
+#print(teta_final)
 
-plt.scatter(x,y)
-plt.plot(x, prediction, c='r')
-plt.show()
-'''
+prediction = model.predict(X,y)
+print(prediction)
+
+# Scoring 
+#prediction = model.main_function(X, final)
+#print(model.score(y, prediction))
